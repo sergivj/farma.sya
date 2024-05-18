739c94c20f06e1e4ed5bce70d1cb05504ef11b3b
sergivj <57764967+sergivj@users.noreply.github.com>
2024-05-18 14:02:26 +0200

    Update styles create page

--- app/components/createPage.tsx
+++ app/components/createPage.tsx
@@ -44,10 +44,10 @@ const AddPage = () => {
         <div className="relative overflow-x-none lg:overflow-x-auto">
             <h2 className='font-extrabold uppercase flex justify-center pb-5 text-xl'> Añadir nuevo medicamento</h2>
             <form action={handleClick} className='flex flex-col gap-4 p-4'>
-                <input className='p-4 rounded-md text-black' type={'text'} placeholder={'Nombre'} name={'nombre'} onChange={handleNombre}/>
-                <input className='p-4 rounded-md text-black' type={'text'} placeholder={'Fecha de Caducidad'} name={'fecha'} onChange={handleFecha}/>
-                <input className='p-4 rounded-md text-black' type={'number'} placeholder={'Cantidad'} name={'cantidad'} onChange={handleCantidad}/>
-                <button className='border border-white p-2 rounded-3xl' type={'submit'}>Enviar</button>
+                <input className='bg-color-primary border border-color-secondary font-bold placeholder:font-semibold placeholder-black p-4 rounded-md text-black' type={'text'} placeholder={'Nombre'} name={'nombre'} onChange={handleNombre}/>
+                <input className='bg-color-primary border border-color-secondary font-bold placeholder:font-semibold placeholder-black p-4 rounded-md text-black' type={'text'} placeholder={'Fecha de Caducidad'} name={'fecha'} onChange={handleFecha}/>
+                <input className='bg-color-primary border border-color-secondary font-bold placeholder:font-semibold placeholder-black p-4 rounded-md text-black' type={'number'} placeholder={'Cantidad'} name={'cantidad'} onChange={handleCantidad}/>
+                <button className='flex border border-color-secondary justify-center p-2 rounded-lg bg-color-primary hover:bg-[#F1F1F2] font-semibold' type={'submit'}>Enviar</button>
             </form>
         </div>
     );
